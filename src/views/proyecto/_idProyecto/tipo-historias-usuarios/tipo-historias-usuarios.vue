c4b34fd5e1dc6c3fc02ccc12a47093513bc6deae
JessiProgram <jessicala182@gmail.com>
2022-09-22 17:37:24 -0400

    importar tipo HU

--- src/views/proyecto/_idProyecto/tipo-historias-usuarios/tipo-historias-usuarios.vue
+++ src/views/proyecto/_idProyecto/tipo-historias-usuarios/tipo-historias-usuarios.vue
@@ -45,7 +45,7 @@
                 </tbody>
             </v-simple-table>
             <h4 class="mt-3">Buscar e importar Tipo de Historia de Usuario de otros proyectos</h4>
-            <v-btn outlined @click="importarTiposHU()" color="indigo">
+            <v-btn outlined @click="openDialogImportar()" color="indigo">
                 Buscar e Importar
             </v-btn>
         </v-container>
@@ -99,6 +99,71 @@
             </v-card>
         </v-dialog>
 
+         <!-- dialog de importar-->
+         <v-dialog
+            v-model="dialogImportarTipoHU"
+            max-width="800px"
+        >
+            <v-card>
+                <v-card-title class="informacionAccion textoInformacionAccion">
+                    Tu lista de proyectos en los que participas y sus Tipos de Historia de Usuario
+                </v-card-title>
+
+                <v-container>
+
+                    <v-expansion-panels>
+                        <v-expansion-panel
+                        v-for="(proyecto,i) of listaProyectos"
+                        :key="i"
+                        >
+                            <div v-if="Number(proyecto.pk) !== Number(idProyecto)">
+                                <v-expansion-panel-header @click="buscarTiposHU(proyecto)">
+                                    <h4>{{proyecto.fields.nombre}}</h4>
+                                </v-expansion-panel-header>
+                                <v-expansion-panel-content>
+                                    <v-list two-line>
+                                        <v-list-item-group
+                                            multiple
+                                        >
+                                            <div v-for="(tipoHU, index) in listaTiposHUImportar">
+                                                <v-list-item :key="tipoHU.fields.nombre">
+                                                    <v-list-item-content>
+                                                        <v-list-item-title v-text="tipoHU.fields.nombre"></v-list-item-title>
+                                                    </v-list-item-content>
+
+                                                    <v-list-item-action>
+                                                        <v-btn outlined :loading="importando" @click="importarTiposHU(proyecto, tipoHU)" color="indigo">
+                                                            Importar
+                                                        </v-btn>
+                                                    </v-list-item-action>
+                                                </v-list-item>
+
+                                                <v-divider
+                                                    v-if="index < listaTiposHUImportar.length - 1"
+                                                    :key="index"
+                                                ></v-divider>
+                                            </div>
+                                        </v-list-item-group>
+                                    </v-list>
+                                    
+                                </v-expansion-panel-content>
+                            </div>
+                        </v-expansion-panel>
+                    </v-expansion-panels>
+
+                    <v-card-actions class="d-flex flex-row-reverse pb-5 pt-5">
+                        <v-btn
+                            color="grey darken-2"
+                            text
+                            @click="dialogImportarTipoHU = false"
+                        >
+                            Cerrar
+                        </v-btn>
+                    </v-card-actions>
+                </v-container>
+            </v-card>
+        </v-dialog>
+
     </LayoutDefault>
 </template>
 <script>
@@ -119,6 +184,11 @@ export default {
             tipoHUSeleccionado: null,
             confirmacionEliminaciontipoHU: "",
             dialogActualizarTipoHU: false,
+
+            dialogImportarTipoHU: false,
+            listaProyectos:[],
+            listaTiposHUImportar: [],
+            importando: false,
 
             columnas: []
         }
@@ -218,7 +288,67 @@ export default {
             this.confirmacionEliminaciontipoHU = ''
         },
 
-        async importarTiposHU(){
+        async openDialogImportar() {
+            this.dialogImportarTipoHU = true
+
+            // buscamos todos proyectos del usuario
+            let idToken = this.$store.state.usuario.idToken
+            // llamamos al backend y solicitamos lo proyectos del usuario
+            // Llamamos al backend
+            let config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${idToken}` 
+                }
+            }
+
+            let res = await axios.get('/usuario/proyectos', config)
+
+            this.listaProyectos = res.data
+
+
+        },
+
+        async buscarTiposHU(proyecto){
+
+            let idToken = this.$store.state.usuario.idToken
+            // listamos tipos de HU
+            let config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${idToken}`
+                }
+            }
+
+            let res = await axios.get(`/tipoHistoriaUsuario/?idproyecto=${proyecto.pk}`, config)
+
+            console.log("res.data", res.data)
+            this.listaTiposHUImportar = res.data
+
+        },
+
+        async importarTiposHU(proyecto, tipoHU){
+            this.importando = true
+
+            const idToken = this.$store.state.usuario.idToken
+
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${idToken}`
+                }
+            }
+
+            const body = {
+                id_tipo_HU: tipoHU.pk,
+                id_proyecto: this.idProyecto,
+            }
+
+            const response = await this.axios.put(`/tipoHistoriaUsuario/`, body, config)
+
+            this.importando = false
+            alert("Tipo de Historia de Usuario Importado")
+            this.inicializarLista()
 
         },
 
@@ -231,11 +361,11 @@ export default {
 <style scoped>
     .informacionAccion {
         /* rgba(230, 62, 62, 0.159) */
-        background-color: rgba(255, 29, 137, 0.159);
+        background-color: rgba(15, 92, 79, 0.159);
     }
     
     .textoInformacionAccion {
-        color: rgb(197, 52, 52);
+        color: rgb(16, 56, 42);
     }
     
     .inputConfirmacionAccion {
