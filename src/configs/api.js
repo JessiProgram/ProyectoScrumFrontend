300628b8d415b3aa8a74d5bfafa22ef7c697fd2d
JessiProgram <jessicala182@gmail.com>
2022-10-17 12:31:31 -0300

    boton deshabilitar

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,5 +1,5 @@
 const api = {
-    url: process.env.NODE_ENV !== 'development' ? 
+    url: process.env.NODE_ENV === 'development' ? 
         'http://localhost:8000/api/v1' : 
         'https://proyectoscrumgrupo5.herokuapp.com/api/v1'
 }
