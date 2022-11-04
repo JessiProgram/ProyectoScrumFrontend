ecf4b819e30f6563a7ad7e958fe6c529627f0dbc
guillepaivag1605 <75756594+guillepaivag1605@users.noreply.github.com>
2022-11-04 13:14:20 -0300

    Rediseño del scroll.

--- src/App.vue
+++ src/App.vue
@@ -22,3 +22,23 @@ export default {
   }
 };
 </script>
+
+<style>
+::-webkit-scrollbar {
+  width: 9px;
+}
+
+::-webkit-scrollbar-track {
+  background-color: #dbdbdb;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #3e9e4f;
+  border-radius: 20px;
+}
+
+::-webkit-scrollbar:horizontal{
+  height: 9px;
+  background-color: blue;
+}
+</style>
